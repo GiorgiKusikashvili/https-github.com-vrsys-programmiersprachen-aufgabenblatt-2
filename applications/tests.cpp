1e59c742fde21364dd11ebb6eb73e29f9008a145
Adrian Kreskowski <adrian.kreskowski@uni-weimar.de>
2026-03-31 14:22:47 +0200

    Add github workflows, badges  and tests

--- applications/tests.cpp
+++ applications/tests.cpp
@@ -1,6 +1,23 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include <doctest.h>
 
+int factorial(int n)
+{
+  int result = 1;
+  for (int i = 2; i <= n; ++i) {
+    result *= i;
+  }
+  return result;
+}
+
+TEST_CASE("factorial computes n!")
+{
+  CHECK(factorial(0) == 1);
+  CHECK(factorial(1) == 1);
+  CHECK(factorial(5) == 120);
+  CHECK(factorial(10) == 3628800);
+}
+
 int main(int argc, char *argv[])
 {
   doctest::Context ctx;
